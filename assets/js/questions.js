c5bdeb6865394446dcc8f8bd4bf53cc78ea26480
JamieCollins <Teyouneek@hotmail.com>
2022-11-03 18:59:30 +0000

    add multiple questions an begin javascript by targetting elements required

---
+++ assets/js/questions.js
@@ -0,0 +1,20 @@
+let questions = [
+    {
+        number: 1,
+        question: "For what movie did Steven Spielberg win his first Oscar for Best Director?",
+        options: ["Saving Private Ryan", "E.T.", "Jaws", "Schindler's List"],
+        answer: "Schindler's List",
+    },
+    {
+        number: 2,
+        question: "What Hollywood movie star plays himself in Zombieland?",
+        options: ["Tom Hanks", "Bill Murray", "Liam Neeson", "Pierce Brosnan"],
+        answer: "Bill Murray",
+    },
+    {
+        number: 3,
+        question: "Who is the only actor to receive an Oscar nomination for acting in a Lord of the Rings movie?",
+        options: ["Ian McKellen", "Elijah Wood", "Viggo Mortensen", "Orlando Bloom"],
+        answer: "Ian McKellen",
+    }
+]
---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let questionText = document.getElementsByClassName("quiz-que").innerHTML;
+document.getElementById("submit").addEventListener("click", getQuestion);
+document.getElementsByClassName("answer-area").addEventListener("click", getQuestion);
+
+function getQuestion() {
+    console.log(hi);
+}
