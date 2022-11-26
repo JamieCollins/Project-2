00a94fe3a842aba0a58505d3393b05de9a77a899
JamieCollins <Teyouneek@hotmail.com>
2022-11-26 19:24:45 +0000

    add a question counter to quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,6 @@ var response = "";
 
 // retrieves stored high score from browser on load
 function getHighScore() {
-
     storedHighScore = localStorage.getItem("highScore");
     if (storedHighScore != undefined){
         highScore.innerHTML = storedHighScore;
@@ -35,10 +34,8 @@ function getHighScore() {
 
 // shuffles questions, loads game
 function startGame () {
-
     shuffleArray(questions);
     getQuestion();
-
 }
 
 // shuffles question array to randomise question order
@@ -49,9 +46,8 @@ function shuffleArray(array) {
     }
 }
 
-// gets question from array via index  
+// gets question from array via index, sets data for question counter
 function getQuestion () {
-
     document.getElementById("start-screen").style.display = "none";
     document.getElementById("game").style.display = "block";
     queText.innerHTML = questions[i].question;
@@ -64,64 +60,53 @@ function getQuestion () {
     optionB.style.border = "none";
     optionC.style.border = "none";
     optionD.style.border = "none";
-        
+    document.getElementById("que-number").innerHTML = `${(i+1)} of ${questions.length}`; 
 }
 
 // answer event listeners
 function optionSelect () {
-
     document.getElementById("option-a").addEventListener("click", selectOption);
     document.getElementById("option-b").addEventListener("click", selectOption);
     document.getElementById("option-c").addEventListener("click", selectOption);
     document.getElementById("option-d").addEventListener("click", selectOption);
-
 }
 
 // highlights option selected by user and assigns data to response
 function selectOption() {
-
     clearOptions();
-    
     response = this.innerHTML;
     if (response === questions[i].answer){
         this.style.backgroundColor = "cyan";
     }   else {
         this.style.backgroundColor = "red";
     }
-
     removeOptionSelect();
     selectOptionTimeout();
-
 }
 
 // answer submission timout and correct/incorrect answer highlight
 function selectOptionTimeout() {
-    setTimeout(ansDelay, 100);
+    setTimeout(ansDelay, 1000);
 }
 
 // removes answer event listeners once option is submitted
 function removeOptionSelect () {
-
     document.getElementById("option-a").removeEventListener("click", selectOption);
     document.getElementById("option-b").removeEventListener("click", selectOption);
     document.getElementById("option-c").removeEventListener("click", selectOption);
     document.getElementById("option-d").removeEventListener("click", selectOption);
-
 }
 
 // clears highlighted answer options
 function clearOptions () {
-
     optionA.style.backgroundColor = "#531aff";
     optionB.style.backgroundColor = "#531aff";
     optionC.style.backgroundColor = "#531aff";
     optionD.style.backgroundColor = "#531aff";
-
 }
 
 // answer submission timout and correct/incorrect answer highlight
 function ansDelay () {
-
     if (optionA.innerHTML === questions[i].answer){
         optionA.style.backgroundColor = "green";
     }   else if (optionB.innerHTML === questions[i].answer){
@@ -133,15 +118,12 @@ function ansDelay () {
     }   else {
         alert ("Please choose an answer.");
     }
-
-    setTimeout(checkAnswer, 100);
+    setTimeout(checkAnswer, 1000);
 }
 
 // checks user answer against stored array answer, increments score accordingly, gets next question, clears user answer
 function checkAnswer() {
-
     clearOptions();
-
     if (response === questions[i].answer){
         correctScore.innerHTML++;
         if (correctScore.innerHTML >= highScore.innerHTML){
@@ -160,24 +142,19 @@ function checkAnswer() {
         i++;
         endQuiz();
     } 
-    
     response = "";
-    setTimeout(nextQue, 200);
-    
+    setTimeout(nextQue, 2000);
 }
 
 // Gets next question, displays quiz box, removes answer response
 function nextQue () {
-
     getQuestion();
     document.getElementById("answer-response-cont").style.display = "none";
     document.getElementById("container").style.display = "block";
-    
 }
 
 // checks if current question is the last
 function endQuiz () {
-
     if (i === questions.length){
         document.getElementById("fin-quiz-cont").style.display = "block";
         document.getElementById("answer-response-cont").style.display = "block";
@@ -187,21 +164,18 @@ function endQuiz () {
     } else {
         getQuestion();
     }
-
 }
 
-// ends quiz, displays results, displays restart quiz button
+// ends quiz, displays results and score percentage, displays restart quiz button
 function quizResults () {
-
-    document.getElementById("text-response").innerHTML = `Congratulations you scored ${correctScore.innerHTML} out of ${i}!`;
+    const scorePercent = (correctScore.innerHTML / i * 100);
+    document.getElementById("text-response").innerHTML = `Congratulations you scored ${correctScore.innerHTML} out of ${i}! <br>${scorePercent}%!`;
     finQuiz.innerHTML = "Restart quiz";
     finQuiz.addEventListener("click", resetQuiz);
-
 }
 
 // resets question counter and score and restarts game
 function resetQuiz () {
-
     document.getElementById("answer-response-cont").style.display = "none";
     document.getElementById("container").style.display = "block";
     finQuiz.style.display = "none";
@@ -209,6 +183,4 @@ function resetQuiz () {
     finQuiz.removeEventListener("click", resetQuiz);
     i = 0;
     startGame();
-    
 }
-
